54949fc9ad3345633eacb775bb0bd15b8788db3c
Jeslyn Lai <imlyj5@gmail.com>
2025-03-18 10:35:03 -0700

    nested data structure practice

--- main.py
+++ main.py
@@ -1,2 +1,25 @@
 def calculate_class_averages(classrooms):
-    pass
+    output = {}
+    for topic, student in classrooms.items():
+        scores = 0
+        grade_count = 0
+        if student == {}:
+             output[topic]=0
+        for grades in student.values():
+             for grade in grades:
+                  scores += grade
+                  grade_count += 1
+                  output[topic]=scores/grade_count
+
+    return output
+
+classrooms = {
+    "Math": {},
+    "Science": {
+        "Alice": [80, 85, 88],
+        "Bob": [78, 82, 85]
+    }
+}
+
+print(calculate_class_averages(classrooms))
+
